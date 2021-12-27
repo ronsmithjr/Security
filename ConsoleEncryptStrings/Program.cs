38f0210c56e73777824b7ae4ad3c40fc28b749fa
Ron Smith <ronald.r.smith.jr@gmail.com>
2021-12-27 11:37:03 -0500

    Adding Front End Items.

---
+++ ConsoleEncryptStrings/Program.cs
@@ -0,0 +1,33 @@
+﻿using EncryptStrings;
+using System;
+
+namespace ConsoleEncryptStrings
+{
+    class Program
+    {
+        static string StringToEncrypt { get; set; }
+        static string StringToDecrypt { get; set; }
+        static string EncryptionKey = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        static void Main()
+        {
+            GetEncryptedValue();
+            GetDecryptedValue();
+            Console.Read();
+        }
+
+        static void GetEncryptedValue()
+        {
+            Console.WriteLine("Enter the string that you want to encrypt: ");
+            StringToEncrypt = Console.ReadLine();
+            var encryptedString = StringToEncrypt.Encrypt(EncryptionKey);
+            Console.WriteLine($"Encrypted String is {encryptedString}");
+        }
+        static void GetDecryptedValue()
+        {
+            Console.WriteLine("Enter the string that you want to decrypt: ");
+            StringToDecrypt = Console.ReadLine();
+            var decryptedString = StringToDecrypt.Decrypt(EncryptionKey);
+            Console.WriteLine($"Decrypted String is {decryptedString}");
+        }
+    }
+}
